12b4634c3794c001e0383e18b2f2d413f9ff5583
alienorar <rayhonakhayrullaeva@gmail.com>
2024-07-10 10:18:59 +0500

    first commit

--- index.js
+++ index.js
@@ -59,3 +59,66 @@
 
 // countLetter("My major is Aerospace engineering but i also like the Web Programming ")
 
+// Task 4
+
+// 4-task
+
+// function minMaxWord(w) {
+//    const words = w.split(" ");
+
+//    let minWord = words[0];
+//    let maxWord = words[0];
+
+//    words.forEach((word) => {
+//       if (word.length < minWord.length) {
+//          minWord = word;
+//       }
+//       if (word.length > maxWord.length) {
+//          maxWord = word;
+//       }
+//    });
+
+//    return { minWord, maxWord };
+// }
+
+// const result = minMaxWord("i am studying Frontend");
+// console.log(result);
+
+
+
+
+
+
+
+
+// Task 5
+
+// let array = ["CHevrolete", "BMw", "Porsche"]
+
+// function addObj(arr) {
+//     let obj = {};
+//     for (let i = 0; i < array.length; i++) {
+//         obj[array[i]] = array[i].length;
+//     }
+//     console.log(obj)
+// }
+// addObj()
+
+// Task 6
+
+// let num = prompt("Enter number");
+// function getLevel() {
+//     let sum = 1;
+//     let arr = [];
+//     for (let i = 1; i <= num; i++) {
+//         sum = sum * 2
+//         arr.push(sum)
+//     }
+//     console.log(arr)
+// }
+
+// getLevel()
+
+// Task 7
+// 7ga tushunmadim uzr
+
